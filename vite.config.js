afe393ebe169e23ada6458ee173637ac96fb3f4e
Eugelito Troyo <eugelito@outlook.com>
2025-07-12 20:38:13 +0100

    fix base path

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-const isProd = process.env.NODE_ENV === "production";
-
 // https://vitejs.dev/config/
 
 export default defineConfig({
-  base: isProd ? "/sendquote-ui/" : "/",
+  base: "/",
   plugins: [react()],
   build: {
-    outDir: "gh-pages",
+    outDir: "docs",
   },
 });
