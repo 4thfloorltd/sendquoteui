44a4d8297214183ce0d980b68f69fe8f03be929c
Eugelito Troyo <eugelito@outlook.com>
2025-07-12 23:14:10 +0100

    use custom domain

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 
 export default defineConfig({
-  base: "./",
+  base: "/",
   plugins: [react()],
-  build: {
-    outDir: "docs",
-  },
 });
